21a78e36665a2049737711a6d83e23e51d598202
Rami Sabbagh <ramilego4game@gmail.com>
2020-12-24 20:24:50 +0200

    Created the maven project

---
+++ src/test/java/org/freestuffbot/api/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package org.freestuffbot.api;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+    @Test
+    void name() {
+        HelloWorld.main(new String[0]);
+    }
+}
